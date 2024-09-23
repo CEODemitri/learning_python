35cbfa90a7d71d662c6331fd321e3652194e0e52
ceoDemitri <demichole@gmail.com>
2024-09-23 05:45:18 -0500

    one extra files prctice: created a file and added content

--- section-6/files/app.py
+++ section-6/files/app.py
@@ -14,3 +14,16 @@ my_file_writing = open('data.txt', 'w')
 my_file_writing.write(user_name)
 
 my_file_writing.close()
+
+
+# self practice
+# create a file
+# open the file for manipulation
+my_file_creating = open('new_data.txt', 'w')
+
+# write to the file to add content
+my_file_creating.write('Hello, Kosmos!')
+
+# ALWAYS CLOSE THE FILE AS SOON AS POSSIBLE
+my_file_creating.close()
+# ran a test run and SUCCESS! FILE WAS CREATED!
